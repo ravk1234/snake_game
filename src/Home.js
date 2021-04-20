1c294168472d9ff81d1c01d68845e013555d1d0a
ravk1234 <rkant4111@gmail.com>
2021-04-20 15:00:41 +0530

    first commit

---
+++ src/Home.js
@@ -0,0 +1,39 @@
+import React,{useState} from "react";
+import "./home.css";
+import Snake from './Snake';
+import Food from "./Food";
+import App from "./App";
+
+function Home() {
+   const [state, setState] = useState('start')
+
+   function openGame() {
+    setState('startGame')
+  };
+
+  return(
+
+    <div className="game-area">
+      {
+        state ='start' && (
+        <>
+        <h1 className="heading"> Snake Game</h1>
+        <button className="para" onClick={this.openGame}>Start Game</button>
+        <button className="para">About</button>
+        <button className="para">Instructions</button>
+        </>)
+      }
+
+      {
+        state='startGame' && (<App/>)
+      }
+
+
+    </div>
+  );
+
+
+}
+
+
+export default Home;
